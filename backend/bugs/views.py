e839eb08b320a356bca0ea65ff4e6fd64b704beb
Kavyashree <kavyashreesaliyan@gmail.com>
2026-09-22 20:28:51 +0530

    Add bug management REST API

--- backend/bugs/views.py
+++ backend/bugs/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Bug
+from .serializers import BugSerializer
 
-# Create your views here.
+
+class BugViewSet(viewsets.ModelViewSet):
+    queryset = Bug.objects.all().order_by("-created_at")
+    serializer_class = BugSerializer
