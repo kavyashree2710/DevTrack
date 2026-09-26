0dc496ac2a67dee6d74ef4f31deab992d4457590
Kavyashree <kavyashreesaliyan@gmail.com>
2026-09-26 22:41:15 +0530

    Add status filtering to bug API

--- backend/bugs/views.py
+++ backend/bugs/views.py
@@ -9,3 +9,13 @@ class BugViewSet(viewsets.ModelViewSet):
     queryset = Bug.objects.all().order_by("-created_at")
     serializer_class = BugSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Bug.objects.all().order_by("-created_at")
+
+        status = self.request.query_params.get("status")
+
+        if status:
+            queryset = queryset.filter(status=status)
+
+        return queryset
